ea44581404c779b4af82da41ab427dd2b731fa92
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-04 23:38:19 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("$", {
+    name: "dollar", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "$", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -24,5 +24,62 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+	upgrades: {
+		11: {
+			title: "Dollar Production",
+    		description: "Starts.",
+    		cost: new Decimal(1),
+        },
+		12: {
+			title: "+2",
+    		description: "whats this",
+    		cost: new Decimal(20),
+        },
+		13: {
+			title: "Triple  Gain",
+    		description: "triple?",
+    		cost: new Decimal(30),
+        },
+		14: {
+			title: "Triple  Gain",
+    		description: "another triple?",
+    		cost: new Decimal(30),
+        },
+		15: {
+			title: "x10 Mulit",
+    		description: "x10 this known",
+    		cost: new Decimal(1000),
+        },
+		21: {
+			title: "x20 Mulit",
+    		description: "did alerting",
+    		cost: new Decimal(1e7),
+        },
+		22: {
+			title: "Increasing for Mulit",
+    		description: "x1e7 this known",
+    		cost: new Decimal(1e12),
+        },
+		23: {
+			title: "x15 mulit",
+    		description: "x15 this known?",
+    		cost: new Decimal(1e17),
+        },
+		24: {
+			title: "Powe^2 of mulit",
+    		description: "x1e10 this known?",
+    		cost: new Decimal(1e25),
+        },
+		31: {
+			title: "Tertate of Mulit",
+    		description: "what dosent xee12519?",
+    		cost: new Decimal("1e400"),
+        },
+		32: {
+			title: "Ultimate Upgrade!?!?!?!?",
+    		description: "to win for double",
+    		cost: new Decimal("eeee308"),
+        },
+    },
 })
